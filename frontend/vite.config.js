4f9ddd666bc73dc6495fb4b7665a082ee9d853ae
0Mouse0 <mauricioraul_96@hotmail.com>
2026-07-13 21:19:59 -0400

    ci:  configuariones para pruebas de coverage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,20 @@ export default defineConfig({
     //environment: 'node', // Para probar funciones puras
     environment: 'jsdom', // Sirve para pruebas de componentes React
     setupFiles: './src/test/setup.js', // Sirve para pruebas de componentes React
+    // Para pruebas de coverage
+    exclude: [
+      '**/node_modules/**',
+      '**/tests/**',
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 60,
+        functions: 60,
+        branches: 50,
+        statements: 60,
+      },
+    },
   },
 })
