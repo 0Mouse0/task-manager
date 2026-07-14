bb88c39fcf855938adcb72fe32b51cd1d772fd41
0Mouse0 <mauricioraul_96@hotmail.com>
2026-07-13 22:02:19 -0400

    fix: corregir caso culpable, todos los checks en verde

--- frontend/tests/flujo-tareas.spec.ts
+++ frontend/tests/flujo-tareas.spec.ts
@@ -7,8 +7,8 @@ test('un usuario puede crear una tarea y verla en la lista', async ({ page }) =>
     await page.getByRole('button', { name: 'Agregar Tarea' }).click()
 
     // Texto Correcto
-    // await expect(page.getByText('Comprar pan')).toBeVisible()
+    await expect(page.getByText('Comprar pan')).toBeVisible()
 
     // ! Provocando error
-    await expect(page.getByText('Comprar pan 🚫 texto incorrecto')).toBeVisible()
+    // await expect(page.getByText('Comprar pan 🚫 texto incorrecto')).toBeVisible()
 })
