09aad8b035648b079b9ad82435b45788f5019e62
0Mouse0 <mauricioraul_96@hotmail.com>
2026-07-17 21:21:10 -0400

    Fix: agregando excepción al lint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -7,4 +7,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   tseslint.configs.recommended,
+  {
+    files: ["prisma/seed.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ]);
