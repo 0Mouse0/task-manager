b29590a4ccad4142cec7fb914cda44c9dcbf8997
0Mouse0 <mauricioraul_96@hotmail.com>
2026-07-17 21:15:15 -0400

    Regresando a const

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+const { PrismaClient } = require('@prisma/client')
+const { PrismaPg } = require('@prisma/adapter-pg')
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
